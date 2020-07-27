6f58ff468c8e190060a8e6e7a3002515b04deb08
root <root@dvps3.ru-central1.internal>
2020-07-27 20:08:53 +0000

    Init commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
+mvn package -f boxfuse-sample-java-war-hello
+cp boxfuse-sample-java-war-hello/target/hello-1.0.war /opt/artifact/
