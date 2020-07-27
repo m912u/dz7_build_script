a9be579f7257c6594f22ce71861b12bd71cc35ac
m912u <golostenov@yandex.ru>
2020-07-27 23:51:39 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Скрипт-сборщик. Вызывается из контейнера maven-build-farm . Принимает единственным параметром каталог, в котором должны быть размещены артефакты сборки.
 git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
 mvn package -f boxfuse-sample-java-war-hello
 cp boxfuse-sample-java-war-hello/target/hello-1.0.war $1
