50d693e8351ee6c8c1d7b014e0fb8694023fb847
m912u <golostenov@yandex.ru>
2020-07-27 23:12:40 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
-mvn package -f boxfuse-sample-java-war-hello
-cp boxfuse-sample-java-war-hello/target/hello-1.0.war /opt/artifact/
+git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git ./
+mvn package clean
+cp target/hello-1.0.war $1
